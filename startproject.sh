c291d754a78995db3990bf731ecbee768471e1f4
jams2 <joshamunn@gmail.com>
2018-07-03 15:49:16 +0100

    update startproject.sh

--- startproject.sh
+++ startproject.sh
@@ -32,8 +32,28 @@ sudo chown -R $USER:$USER .
 echo "[!] Ownership granted to ${USER}."
 
 echo "[+] Updating ${projectname}/settings.py to use postgresql..."
-cat $projectname/settings.py | sed -e "78s/sqlite3/postgresql" > $projectname/tmp
+cat $projectname/settings.py | sed -e "78s/sqlite3/postgresql/" > $projectname/tmp
 mv $projectname/tmp $projectname/settings.py
-cat $projectname/settings.py | sed -e "79s/os.path.join(BASE_DIR, 'db.sqlite3')/'postgres'" > $projectname/tmp
+cat $projectname/settings.py | sed -e "79s/os.path.join(BASE_DIR, 'db.sqlite3')/'postgres'/" > $projectname/tmp
 mv $projectname/tmp $projectname/settings.py
 sed -i "80i\ \ \ \ \ \ \ \ 'USER': 'postgres',"
+sed -i "81i\ \ \ \ \ \ \ \ 'HOST': 'db',"
+sed -i "82i\ \ \ \ \ \ \ \ 'PORT': 5432,"
+
+echo "[+] Making initial migrations..."
+docker-compose run web python /django/manage.py mikemigrations
+docker-compose run web python /django/manage.py migrate
+
+echo "[!] Done. Create Django superuser? [Y/n]"
+read choice
+if [ -z $choice ] || [ $choice == "y" ]; then
+    docker-compose run web python /django/manage.py createsuperuser
+fi
+
+echo "[!] Initial project setup complete. Run development server?"
+read choice
+if [ -z $choice ] || [ $choice == "y" ]; then
+    docker-compose up
+else
+    echo "Quitting..."
+fi
